57ccf9a26136ef6d496d2c90eceb4afabc677d51
鲁正佼 <3038413299@qq.com>
2022-09-20 17:20:30 +0800

    feat(custom): 配置浏览器标题 ico图标 端口号 serve 服务器

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer : {
+    // 端口号
+    port : 8888,
+    // 主机名
+    host : "localhost",
+    // 项目启动时是否自动打开浏览器
+    open : true,
+    // 是否开启https
+    https : false
+  }
 })
