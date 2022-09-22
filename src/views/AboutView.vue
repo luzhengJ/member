a6d0ccd9f34edb7059af65e28313af18ced61788
鲁正佼 <3038413299@qq.com>
2022-09-22 16:56:23 +0800

    feat(custom): 实现登录页面布局

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,80 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <ul>
+    <li>111</li>
+   </ul>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+// import axios from "axios";
+// import request from "../utils/request.js"
+ import Testapi from '../api/test.js'
+
+export default {
+  name: "HomeView",
+  data () {
+      return {
+         page:1,
+         size:10,
+         test:{
+          name:"jack"
+         }   ,
+         id:100
+      }
+  },
+  components: {},
+  created() {
+    // 会员管理
+    Testapi.getMemberList( this.page, this.size ,this.test).then(response=>{
+      console.log('member=>',response);
+    }).catch(error=>{
+      console.log(error);
+    })
+
+    // 供应商管理
+    Testapi.getSupplier( this.page, this.size ).then(response=>{
+      console.log('Supplier=>',response);
+    }).catch(error=>{
+      console.log(error);
+    })
+
+    // 查询会员
+    Testapi.findMenber(this.id).then(response=>{
+      console.log('find=>',response);
+    }).catch(error=>{
+      console.log(error);
+    })
+
+
+
+
+
+
+
+
+    // // console.log('promise',Testapi.getNewList());
+    // Testapi.getNewList().then(response=>{
+    //    console.log(response);
+    // }).catch(error=>{
+    //    console.log(error);
+    // })
+    // request({
+    //   url: process.env.VUE_APP_BASE_API+"/",
+    //   method: "GET",
+    // }).then(response => {
+    //     console.log(response);
+    //   }).catch(error => {
+    //     console.log(error);
+    //   }),
+      // request({
+      //   url: process.env.VUE_APP_BASE_API1+"/",
+      //   method: "GET",
+      // }).then(response => {
+      //     console.log(response);
+      //   }).catch(error => {
+      //     console.log(error);
+      //   });
+  },
+};
+</script>
---
+++ src/views/Login/login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <div class="login-from">
+      <h2 class="login-title">积云会员管理系统</h2>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="账号">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        password:"",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.login {
+  height: 100%;
+  background: url("http://vue.mengxuegu.com/img/login.b665435f.jpg") no-repeat;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .login-from {
+    width: 350px;
+    background-color: rgba(255, 255, 255, 0.8);
+    margin: 180px auto;
+    border-radius: 20px;
+    padding: 28px;
+    .login-title{
+        text-align: center;
+        font-size:24px;
+        color: #303133;
+        padding: 20px;
+    }
+  }
+}
+</style>
