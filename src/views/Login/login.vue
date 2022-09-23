d0d8eb9a60fd4e609ea948b05d880ad9e86c83ba
鲁正佼 <3038413299@qq.com>
2022-09-23 22:15:53 +0800

    feat(custom): jkjk

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -11,6 +11,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="handelLoginSubmit">登录</el-button>
+          <el-button type="primary" @click="handelLoginSubmit">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
